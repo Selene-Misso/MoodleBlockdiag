c61f88a86b4503ba7a5b9ed73a6931e4ede4e6cc
Misso <mishiro.24@gmail.com>
2015-01-15 20:13:19 +0900

    Update block_blockdiag.php

--- lang/en/block_blockdiag.php
+++ lang/en/block_blockdiag.php
@@ -1,5 +1,5 @@
 <?php
-$string['pluginname'] = 'Simple HTML block block diag';
+$string['pluginname'] = 'The Blockdiag Editor';
 $string['blockdiag'] = 'Simple HTML';
 $string['blockdiag:addinstance'] = 'Add a new simple HTML block';
 $string['blockdiag:myaddinstance'] = 'Add a new simple HTML block to the My Moodle page';

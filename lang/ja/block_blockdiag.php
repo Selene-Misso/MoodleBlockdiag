7ee8b1d6c3c3086f03cfb05c022ab4fa20252391
Misso <mishiro.24@gmail.com>
2015-01-21 18:08:08 +0900

    Create block_blockdiag.php

---
+++ lang/ja/block_blockdiag.php
@@ -0,0 +1,5 @@
+<?php
+$string['pluginname'] = 'Blockdiag エディタ';
+$string['blockdiag'] = 'Blockdiag エディタ';
+$string['blockdiag:addinstance'] = 'Add a new simple HTML block';
+$string['blockdiag:myaddinstance'] = 'Add a new simple HTML block to the My Moodle page';

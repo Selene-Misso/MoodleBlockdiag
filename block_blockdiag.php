77888664ee845d36c5ab67f7ccc7e5c8b3b1382e
Selene-Misso <selene.jaxa@gmail.com>
2015-01-29 18:45:42 +0900

    入力フォームのテキスト幅を変更

--- block_blockdiag.php
+++ block_blockdiag.php
@@ -15,7 +15,7 @@ class block_blockdiag extends block_base {
 		$this->content         = new stdClass;
 		$this->content->text   = '<div style="text-align: center;">';
 		$this->content->text  .= '<form mothod="post">入力フォーム<br>';
-		$this->content->text  .= '<textarea name="command" rows="4" cols="40">';
+		$this->content->text  .= '<textarea name="command" rows="4">';
 		$this->content->text  .= '</textarea><br><input type="submit" value="変換">';
 		$this->content->text  .= '<br><hr>出力された図<br>';
 		$this->content->text  .= '<form mothod="post"><input type="submit" value="保存"></form>';

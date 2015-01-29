e76c1fcd5e69af1ddd11f787b76871bbef7c5ca0
Selene-Misso <selene.jaxa@gmail.com>
2015-01-29 18:42:26 +0900

    HTMLを表示する

--- block_blockdiag.php
+++ block_blockdiag.php
@@ -13,7 +13,14 @@ class block_blockdiag extends block_base {
 		}
 	 
 		$this->content         = new stdClass;
-		$this->content->text   = $this->config->text;
+		$this->content->text   = '<div style="text-align: center;">';
+		$this->content->text  .= '<form mothod="post">入力フォーム<br>';
+		$this->content->text  .= '<textarea name="command" rows="4" cols="40">';
+		$this->content->text  .= '</textarea><br><input type="submit" value="変換">';
+		$this->content->text  .= '<br><hr>出力された図<br>';
+		$this->content->text  .= '<form mothod="post"><input type="submit" value="保存"></form>';
+		$this->content->text  .= '</form></div>';
+//		$this->content->text  .= '';
 		$this->content->footer = 'これはフッタ';
 
 		return $this->content;

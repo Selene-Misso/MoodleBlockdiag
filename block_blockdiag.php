0f67bd900e8c2da20a137c54351cfdd1fdb5a3de
Selene-Misso <selene.jaxa@gmail.com>
2015-01-29 17:51:40 +0900

    config 追加．

--- block_blockdiag.php
+++ block_blockdiag.php
@@ -1,26 +1,29 @@
 <?php
 class block_blockdiag extends block_base {
-  public function init() {
-    $this->title = get_string('blockdiag', 'block_blockdiag');
-  }
+	public function init() {
+		$this->title = get_string('blockdiag', 'block_blockdiag');
+	}
+	
 // The PHP tag and the curly bracket for the class definition 
 // will only be closed after there is another function added in the next section.
 
-public function get_content() {
-    if ($this->content !== null) {
-      return $this->content;
-    }
- 
-    $this->content         =  new stdClass;
-    $this->content->text   = $this->config->text;
-    $this->content->footer = '';
- 
-    return $this->content;
-  }
-  
-function instance_allow_config() {
-  return true;
+	public function get_content() {
+		if ($this->content !== null) {
+			return $this->content;
+		}
+	 
+		$this->content         = new stdClass;
+		$this->content->text   = $this->config->text;
+		$this->content->footer = '';
+
+		return $this->content;
+	}
+
+
+	function instance_allow_config() {
+		return true;
+	}
 }
-}   // Here's the closing bracket for the class definition
+// Here's the closing bracket for the class definition
 
 ?>

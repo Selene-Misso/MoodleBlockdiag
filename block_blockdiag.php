138498b9cda08c48bb1a1e02d8458a1e989954d4
Selene-Misso <selene.jaxa@gmail.com>
2015-01-30 14:24:58 +0900

    name指定が間違っていた．

--- block_blockdiag.php
+++ block_blockdiag.php
@@ -15,8 +15,11 @@ class block_blockdiag extends block_base {
 		$this->content         = new stdClass;
 		$this->content->text   = '<div style="text-align: center;">';
 		$this->content->text  .= '<form action="../blocks/blockdiag/convert.php" method="post" target="_blank">blockdiagのコマンドを入力<br>';
-		$this->content->text  .= '<textarea name="command" rows="4" style="width: 100%;font-size: 12pt; font-family: monospace;">';
-		$this->content->text  .= '{\n\ta -> b -> c;\n\t     b -> e;\n}';
+		$this->content->text  .= '<textarea name="block" rows="4" style="width: 100%;font-size: 12pt; font-family: monospace;">';
+		$this->content->text  .= '{';
+		$this->content->text  .= 'a -> b -> c -> f;';
+		$this->content->text  .= '     b -> d -> f;';
+		$this->content->text  .= '}';
 		$this->content->text  .= '</textarea><br><input type="submit" value="変換">';
 		$this->content->text  .= '</form></div>';
 		$this->content->footer = 'blockdiagの説明は<a href="http://blockdiag.com/ja/index.html" target"_blank">こちら</a>';
